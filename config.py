80805779fa907ae29ecba87504d8d97923fc1e40
orvice <orvice@gmail.com>
2018-08-18 02:40:31 +0800

    add env file support

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import os
+from dotenv import load_dotenv
+
+
+def load_env_from_file():
+    env_file = os.getenv('ENV_FILE', None)
+    if env_file is not None:
+        load_dotenv(dotenv_path=env_file)
+
+
+load_env_from_file()
 
 # Config
-API_INTERFACE = 'MuApiV2' #mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, MuApiV2
+API_INTERFACE = 'MuApiV2'  # mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, MuApiV2
 
 mu_uri = os.getenv('MU_URI', '')
 node_id = os.getenv('MU_NODE_ID', 1)
